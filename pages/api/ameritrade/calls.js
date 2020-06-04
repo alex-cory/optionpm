fa6605398c95a528ddcec87a04a79306a00650e3
Alex Cory <results@alexcory.com>
2020-06-04 16:46:57 -0700

    cleanup

--- pages/api/ameritrade/calls.js
+++ pages/api/ameritrade/calls.js
@@ -7,8 +7,8 @@ const apiKey = 'TWH9TZESBMMN3WLBALF5R7KPDAMRSECA'
 function getClosestNumber(needle, haystack) {
   return Object.keys(haystack).reduce((prev, curr) => Math.abs(curr - needle) < Math.abs(prev - needle) ? curr : prev)
 }
+
 export default async (req, res) => {
-  // console.log('req.query', req.query)
   const { symbol, discounts, tradeAmount } = req.query
   const query = stringify({
     symbol,
@@ -18,7 +18,6 @@ export default async (req, res) => {
     // interval: 1,
     // fromDate: moment().add(2, 'days').format()
   })
-  // console.log('RES', res)
   try {
     const response = await fetch(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${apiKey}&${query}`)
     const data = await response.json()
@@ -38,58 +37,6 @@ export default async (req, res) => {
         return acc
       }, {})
     })
-    // return res.json(data)
-    // return res.json({
-    //   ...data,
-    //   currentMarketValue,
-    //   callExpDateMap: Object.entries(data.callExpDateMap).reduce((acc, [key, val], i) => i < 6 ? ({ ...acc, [key]: val }) : acc, {})
-    // })
-    const currentMarketValue = data.underlyingPrice.toFixed(2)
-    const staticContractsCount = tradeAmount / (currentMarketValue * 100)
-    if (typeof data.callExpDateMap !== 'object') {
-      console.log('symbol', symbol)
-      console.log('data', data)
-      console.log('data.callExpDateMap!!!', data.callExpDateMap)
-    } 
-    const rows = discounts.map((decimalPercent, i) => {
-      const percent = decimalPercent * 100
-      const discountedStrikePrice = (1 - decimalPercent) * currentMarketValue
-      const callOptionChains = Object.entries(data.callExpDateMap).map(([expDate, callsByStrikePrice]) => {
-        const contractsCount = staticContractsCount // here for when we do dynamic
-        const closestStrikePrice = getClosestNumber(discountedStrikePrice, callsByStrikePrice)
-        // TODO: what if we have multiple calls
-        const closestCall = callsByStrikePrice[closestStrikePrice][0]
-        const closestBid = closestCall.bid * 100 * contractsCount
-        let { daysToExpiration } = closestCall || {}
-        return {
-          closestBid,
-          closestBidPerDay: closestBid / (daysToExpiration || 1),
-          daysToExpiration,
-          expDate
-        }
-      })
-      return {
-        percent,
-        discountedStrikePrice,
-        callOptionChains
-      }
-    })
-    const expirations = Object.entries(data.callExpDateMap).map(
-      ([expDate, { daysToExpiration }]) => ({
-        date: expDate,
-        days: daysToExpiration
-      })
-    )
-    const formatted = {
-      currentMarketValue,
-      rows,
-      expirations
-      // callOptionChains: discounts.map((percent, i) => {
-
-      // })
-      // callExpDateMap: Object.entries(data.callExpDateMap).reduce((acc, [key, val], i) => i < 6 ? ({ ...acc, [key]: val }) : acc, {})
-    }
-    res.json(formatted)
   } catch (err) {
     console.log('ERR', err)
   }
--- pages/api/ameritrade/puts.js
+++ pages/api/ameritrade/puts.js
@@ -8,7 +8,6 @@ function getClosestNumber(needle, haystack) {
   return Object.keys(haystack).reduce((prev, curr) => Math.abs(curr - needle) < Math.abs(prev - needle) ? curr : prev)
 }
 export default async (req, res) => {
-  // console.log('req.query', req.query)
   const { symbol, discounts, tradeAmount } = req.query
   const query = stringify({
     symbol,
@@ -18,7 +17,6 @@ export default async (req, res) => {
     // interval: 1,
     // fromDate: moment().add(2, 'days').format()
   })
-  // console.log('RES', res)
   try {
     const response = await fetch(`https://api.tdameritrade.com/v1/marketdata/chains?apikey=${apiKey}&${query}`)
     const data = await response.json()
@@ -38,58 +36,6 @@ export default async (req, res) => {
         return acc
       }, {})
     })
-    // return res.json(data)
-    // return res.json({
-    //   ...data,
-    //   currentMarketValue,
-    //   putExpDateMap: Object.entries(data.putExpDateMap).reduce((acc, [key, val], i) => i < 6 ? ({ ...acc, [key]: val }) : acc, {})
-    // })
-    const currentMarketValue = data.underlyingPrice.toFixed(2)
-    const staticContractsCount = tradeAmount / (currentMarketValue * 100)
-    if (typeof data.putExpDateMap !== 'object') {
-      console.log('symbol', symbol)
-      console.log('data', data)
-      console.log('data.putExpDateMap!!!', data.putExpDateMap)
-    } 
-    const rows = discounts.map((decimalPercent, i) => {
-      const percent = decimalPercent * 100
-      const discountedStrikePrice = (1 - decimalPercent) * currentMarketValue
-      const putOptionChains = Object.entries(data.putExpDateMap).map(([expDate, putsByStrikePrice]) => {
-        const contractsCount = staticContractsCount // here for when we do dynamic
-        const closestStrikePrice = getClosestNumber(discountedStrikePrice, putsByStrikePrice)
-        // TODO: what if we have multiple puts
-        const closestPut = putsByStrikePrice[closestStrikePrice][0]
-        const closestBid = closestPut.bid * 100 * contractsCount
-        let { daysToExpiration } = closestPut || {}
-        return {
-          closestBid,
-          closestBidPerDay: closestBid / (daysToExpiration || 1),
-          daysToExpiration,
-          expDate
-        }
-      })
-      return {
-        percent,
-        discountedStrikePrice,
-        putOptionChains
-      }
-    })
-    const expirations = Object.entries(data.putExpDateMap).map(
-      ([expDate, { daysToExpiration }]) => ({
-        date: expDate,
-        days: daysToExpiration
-      })
-    )
-    const formatted = {
-      currentMarketValue,
-      rows,
-      expirations
-      // putOptionChains: discounts.map((percent, i) => {
-
-      // })
-      // putExpDateMap: Object.entries(data.putExpDateMap).reduce((acc, [key, val], i) => i < 6 ? ({ ...acc, [key]: val }) : acc, {})
-    }
-    res.json(formatted)
   } catch (err) {
     console.log('ERR', err)
   }
