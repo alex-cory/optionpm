121ae874b68986b0262e549256c5b0afd1b1e00d
Alex Cory <results@alexcory.com>
2020-06-09 11:29:35 -0400

    fixing logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,14 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
-import Box from '@material-ui/core/Box'
-import Container from '@material-ui/core/Container'
 import { Slide, Tabs, Tab } from '@material-ui/core'
-import Link from 'next/link'
-import { useRouter, push } from 'next/router'
+import { useRouter } from 'next/router'
 import styled from 'styled-components'
 
 function HideOnScroll(props) {
@@ -38,23 +34,20 @@ HideOnScroll.propTypes = {
 const routes = { '/': 0, '/calls': 1, '/top-companies': 2 }
 const NavLink = props => {
   const { push } = useRouter()
-  return (
-    // <Link href={props.to}>
-      <Tab onClick={() => push(props.to)} label={props.children} id={routes[props.to]} />
-    // </Link>
-  )
+  return <Tab onClick={() => push(props.to)} label={props.children} id={routes[props.to]} />
+ 
 }
 const TheTabs = styled(Tabs)`
   .MuiTabs-indicator {
     background-color: white !important;
   }
 `
-const Img = styled.div`
-  display: block;
-  max-width:230px;
-  max-height:95px;
-  width: auto;
-  height: auto;
+const Img = styled.img`
+  box-sizing: border-box;
+  padding: 10px;
+  width: 100px;
+  margin-left: auto;
+  filter: brightness(0) invert(1);
 `
 export default function NavBar(props) {
   const { pathname } = useRouter()
@@ -63,29 +56,15 @@ export default function NavBar(props) {
       <CssBaseline />
       <HideOnScroll {...props}>
         <AppBar>
-          {/* <Toolbar> */}
-            <TheTabs value={routes[pathname]}>
-              <NavLink to='/'>Puts</NavLink>
-              <NavLink to='/calls'>Calls</NavLink>
-              {/* <NavLink to='/top-companies'>Top Companies</NavLink> */}
-              <img style={{ marginLeft: 'auto' }} width={120} src='/static/option-pricing-machine.png' />
-            </TheTabs>
-          {/* </Toolbar> */}
+          <TheTabs value={routes[pathname]}>
+            <NavLink to='/'>Puts</NavLink>
+            <NavLink to='/calls'>Calls</NavLink>
+            {/* <NavLink to='/top-companies'>Top Companies</NavLink> */}
+            <Img src='/static/option-pricing-machine.png' />
+          </TheTabs>
         </AppBar>
       </HideOnScroll>
       <Toolbar />
-      {/* <Container>
-        <Box my={2}>
-          {[...new Array(12)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-        </Box>
-      </Container> */}
     </React.Fragment>
   )
 }
